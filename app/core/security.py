4f790bc71194f218b069d6ff490e37412ea3f496
purnasrinivaskoppisetti <purnasrinivaskoppisetti@gmail.com>
2026-04-18 18:25:47 +0530

    second push

--- app/core/security.py
+++ app/core/security.py
@@ -5,7 +5,7 @@ from jose import JWTError, jwt
 from passlib.context import CryptContext
 from app.core.config import settings
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
 
 # 🔐 Hash password
 def hash_password(password: str) -> str:
